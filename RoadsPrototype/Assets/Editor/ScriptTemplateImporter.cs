30835a9512c9b2c812843c26b10d0f5f0954c86a
rohunb <rohunb@gmail.com>
2015-05-29 21:12:06 -0400

    added script template importer

---
+++ RoadsPrototype/Assets/Editor/ScriptTemplateImporter.cs
@@ -0,0 +1,36 @@
+﻿/*
+  ScriptTemplateImporter.cs
+  CitySimulator
+  Created by Rohun Banerji on May 29/2015.
+  Copyright (c) 2015 Rohun Banerji. All rights reserved.
+*/
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+public class ScriptTemplateImporter : UnityEditor.AssetModificationProcessor 
+{
+    public static void OnWillCreateAsset(string path)
+    {
+        //not a script
+        if (!path.Contains(".cs")) return;
+        path = path.Replace(".meta", "");
+        int index = path.LastIndexOf(".");
+        string ext = path.Substring(index);
+        if (ext != ".cs") return;
+        index = Application.dataPath.LastIndexOf("Assets");
+        path = Application.dataPath.Substring(0, index) + path;
+        string file = File.ReadAllText(path);
+        file = file.Replace("#CREATIONDATE#", DateTime.Now.ToString("MMMM dd, yyyy"));
+        file = file.Replace("#PROJECTNAME#", PlayerSettings.productName);
+        file = file.Replace("#YEAR#", DateTime.Now.Year.ToString());
+        File.WriteAllText(path, file);
+        Debug.Log("Injecting copyright info: " +path.Replace(Application.dataPath, ""));
+        AssetDatabase.Refresh();
+    }
+
+}
